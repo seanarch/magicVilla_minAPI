3f650f315ac7c7a0ce962e515f1e319bdb115948
seanarch <zhuqingyun@outlook.com>
2023-06-02 14:58:55 -0600

    Implement automapper

---
+++ MagicVilla_minAPI/MappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using AutoMapper;
+using MagicVilla_minAPI.Models;
+using MagicVilla_minAPI.Models.DTO;
+
+namespace MagicVilla_minAPI
+{
+	public class MappingConfig : Profile
+	{
+		 public MappingConfig()
+		{
+			CreateMap<Coupon, CouponCreateDTO>().ReverseMap();
+            CreateMap<Coupon, CouponDTO>().ReverseMap();
+        }
+	}
+}
+
