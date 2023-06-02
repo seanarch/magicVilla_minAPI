aef88f38e2c8216731b862aba54cef2b3d3aca4b
seanarch <zhuqingyun@outlook.com>
2023-06-02 11:24:06 -0600

    Added data and model

---
+++ MagicVilla_minAPI/Data/CouponStore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MagicVilla_minAPI.Models;
+
+namespace MagicVilla_minAPI.Data
+{
+	public static class CouponStore
+	{
+		public static List<Coupon> couponList = new List<Coupon>
+		{
+			new Coupon{Id=1, Name="10OFF", Percent=10, IsActive=true},
+			new Coupon{Id=1, Name="20OFF", Percent=20, IsActive=false}
+		};
+	}
+}
+
